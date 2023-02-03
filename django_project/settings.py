a1a49a02440befa26220e45473ef12ea5185d46a
Junhyeon Jayden Cho <whqkdhfh13@gmail.com>
2023-02-03 21:49:06 +0000

    push to github from replit, continue in vscode

--- django_project/settings.py
+++ django_project/settings.py
@@ -35,6 +35,7 @@ X_FRAME_OPTIONS = '*'
 # Application definition
 
 INSTALLED_APPS = [
+	'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
